8043f058a28b1711dd1eb14bd9118173248b5545
Bykov Denis Stanislavovich <stivenrain@gmail.com>
2020-04-25 09:29:26 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import Provider from 'react-redux/lib/components/Provider';
+import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
+import reducers from './redux/reducers/combine-reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const middlewares = [
+  thunk,
+  promiseMiddleware({
+    promiseTypeSuffixes: ['LOADING', 'SUCCESS', 'ERROR'],
+  }),
+];
+
+let devTools;
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+  devTools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f;
+} else {
+  devTools = (f) => f;
+}
+
+const store = createStore(reducers, compose(applyMiddleware(...middlewares), devTools));
+
+function AppRoot() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+ReactDOM.render(<AppRoot />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/redux/constants/index.js
@@ -0,0 +1,2 @@
+export const TODO_LIST_ADD_ITEM = 'TODO_LIST_ADD_ITEM';
+export const TODO_LIST_GET_ITEMS = 'TODO_LIST_GET_ITEMS';
